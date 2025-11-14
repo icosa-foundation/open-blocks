a3b844babdb4688721363f5a9c76242261a7cc8f
Andy Baker <andy@andybak.net>
2025-11-14 14:17:19 +0000

    Fix build errors on non-XR platforms

--- Assets/Scripts/model/controller/ControllerDeviceDesktop.cs
+++ Assets/Scripts/model/controller/ControllerDeviceDesktop.cs
@@ -12,11 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using TiltBrush;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.XR;
-using CommonUsages = UnityEngine.XR.CommonUsages;
 
 namespace com.google.apps.peltzer.client.model.controller
 {
