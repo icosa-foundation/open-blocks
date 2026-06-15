ba27450c511e7955c3a4645c81125795117a0d44
Andy Baker <andy@andybak.net>
2026-06-15 16:38:24 +0100

    Fix login / CORS (#156)
    * Add unity MCP
    
    * Match the HTTP config that we have on Open Brush

--- Assets/UserConfig.cs
+++ Assets/UserConfig.cs
@@ -1,18 +1,11 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
 public class UserConfig
 {
-    public static readonly string[] DefaultApiCorsAllowedOrigins =
-    {
-        "https://ixxyxr.github.io" // Used for an example of how a webapp can directly communicate with Open Blocks
-    };
-
     public bool EnableApiRemoteCalls;
-    public string[] ApiCorsAllowedOrigins = (string[])DefaultApiCorsAllowedOrigins.Clone();
+    public bool EnableApiCorsHeaders;
     public string GalleryUrl;
     public string ApiUrl;
 }
