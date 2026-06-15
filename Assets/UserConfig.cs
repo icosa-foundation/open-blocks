e654447b3fb14ba8db4d7d5c30600f0636f0b3e8
Andy Baker <andy@andybak.net>
2026-06-15 23:12:13 +0100

    Allow list for Polyhydra (and others in future)

--- Assets/UserConfig.cs
+++ Assets/UserConfig.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 [Serializable]
 public class UserConfig
 {
+    public static readonly string[] DefaultApiCorsAllowedOrigins =
+    {
+        "https://ixxyxr.github.io" // Used for Polyhydra and other hosted browser integrations
+    };
+
     public bool EnableApiRemoteCalls;
     public bool EnableApiCorsHeaders;
+    public string[] ApiCorsAllowedOrigins = (string[])DefaultApiCorsAllowedOrigins.Clone();
     public string GalleryUrl;
     public string ApiUrl;
 }
