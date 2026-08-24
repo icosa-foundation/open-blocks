5694adc5a551c56e10ef1822083544c1eefad7c3
Andy Baker <andy@andybak.net>
2026-08-24 10:57:22 +0100

    Use Rift secondary button for checkpoint tooltip

--- Assets/Scripts/tools/Freeform.cs
+++ Assets/Scripts/tools/Freeform.cs
@@ -1145,6 +1145,10 @@ namespace com.google.apps.peltzer.client.tools
                 {
                     AddCheckpoint();
                     AddSpine();
+                    if (Config.Instance.VrHardware == VrHardware.Rift)
+                    {
+                        UnsetAllHoverTooltips();
+                    }
                 }
             }
             else if (IsStopScalingEvent(args))
@@ -1171,7 +1175,7 @@ namespace com.google.apps.peltzer.client.tools
             {
                 SetHoverTooltip(peltzerController.controllerGeometry.freeformTooltipRight, TouchpadHoverState.RIGHT);
             }
-            else if (IsSetCenterHoverTooltipEvent(args) && insertionInProgress && isManualCheckpointing)
+            else if (IsSetCheckpointHoverTooltipEvent(args))
             {
                 SetHoverTooltip(peltzerController.controllerGeometry.freeformTooltipCenter, TouchpadHoverState.NONE);
             }
@@ -1374,11 +1378,23 @@ namespace com.google.apps.peltzer.client.tools
               && args.Action == ButtonAction.TOUCHPAD && args.TouchpadLocation == TouchpadLocation.RIGHT;
         }
 
-        private static bool IsSetCenterHoverTooltipEvent(ControllerEventArgs args)
+        private bool IsSetCheckpointHoverTooltipEvent(ControllerEventArgs args)
         {
-            return args.ControllerType == ControllerType.PELTZER
-              && args.ButtonId == ButtonId.Touchpad
-              && args.Action == ButtonAction.TOUCHPAD && args.TouchpadLocation == TouchpadLocation.CENTER;
+            if (!insertionInProgress || !isManualCheckpointing
+              || args.ControllerType != ControllerType.PELTZER)
+            {
+                return false;
+            }
+
+            if (Config.Instance.VrHardware == VrHardware.Rift)
+            {
+                return args.ButtonId == ButtonId.SecondaryButton
+                  && args.Action == ButtonAction.DOWN;
+            }
+
+            return args.ButtonId == ButtonId.Touchpad
+              && args.Action == ButtonAction.TOUCHPAD
+              && args.TouchpadLocation == TouchpadLocation.CENTER;
         }
 
         private static bool IsUnsetAllHoverTooltipsEvent(ControllerEventArgs args)
